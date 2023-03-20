4cfc1a2ce0ec714fcb2004be6f7a96949ca76c0c
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-20 14:20:32 -0700

    remove memory allocation

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,6 @@
 #include "dog.h"
-#include <stdlib.h>
 
-/**
- * init_dog - initialze a varible of type struct dog
+/**init_dog - initialze a varible of type struct dog
  * @d:pointer for struct type
  * @name: pointer of string
  * @age: float type
@@ -11,8 +9,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-
+	
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
