aa2ac6e8789bb455a69dece600d952c4b7a64d35
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-20 14:09:34 -0700

    Includes header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialze a varible of type struct dog
@@ -14,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 
 	if (d == NULL)
 		return (NULL);
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
