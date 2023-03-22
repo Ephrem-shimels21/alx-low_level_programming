cb7ccc78e7a2ef97be2b73306cb1aba386b42fb7
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-22 12:20:16 -0700

    create a function which prints each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+/**
+ * array_iterator - print each element of the array
+ * @array: array of integers
+ * @size: Size of the array
+ * @action: function pointer
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
