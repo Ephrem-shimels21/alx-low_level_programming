a5c8f9cb44e1003ca92ec5d76ab71153a34457fa
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-22 11:43:00 -0700

    create a function which prints a given string name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include<stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: string that is going to printed
+ * @f: ponter to a function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
