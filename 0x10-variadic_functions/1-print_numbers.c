0431b32e4b4feda1646ee968594419f8bedd702c
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-24 13:42:36 -0700

    add space after declaration and after commma

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: a separator which separates numbers
+ * @n: number of integers
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list var;
+	int i;
+
+	va_start(var, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(var, unsigned int);
+
+		printf("%d", num);
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
