c46ac046bf84d1a765baedf121bece4e811d555d
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-24 13:17:14 -0700

    intialize sum and change the conditions of for loop

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list va1;
-	unsigned int i, sum;
+	unsigned int i;
+        int sum = 0;
 
 	va_start(va1, n);
 
-	for (i = va1; i < n; i = va_arg(va1, unsigned int))
+	for (i = 0; i < n; i++)
 	{
-		sum += i;
+		sum += va_arg(va1, unsigned int);
 	}
 	va_end(va1);
 	return (sum);
