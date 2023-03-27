501551cb057c3d9c4a2bcf1617943d6688794ac6
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-27 12:18:59 -0700

    add protypes and define alias for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,10 +5,13 @@
  * @owner:pointer 
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
