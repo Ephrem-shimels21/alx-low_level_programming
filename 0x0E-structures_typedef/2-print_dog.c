a7be3d00e7f7093019e19d75de163f98c6032728
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-27 12:26:10 -0700

    remove space around -> and add spce after ,

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_dog -prints struct dog
@@ -11,10 +12,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d -> name == NULL)
-		d -> name = "(nil)";
-	if (d -> owner == NULL)
-		d -> owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-	printf("Name: %s\n Age: %f\n Owner: %s\n",d -> name, d->age, d->owner);
+	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
