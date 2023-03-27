8acdd2622404298306f1aed49a61a1fcd246f50d
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-27 11:51:35 -0700

    add prototype void init_dog(struct dog *d, char *name, float age, char *owner);

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog -prints struct dog
+ * @d: pointer to the struct dog
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,3 +11,4 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
