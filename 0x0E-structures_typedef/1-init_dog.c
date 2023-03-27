5659db86e969f4dec57df92cbede2b5f630e8c1d
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-27 11:59:38 -0700

    add space and remove unccessary space

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,22 @@
 #include "dog.h"
+#include <stddef.h>
 
-/**init_dog - initialze a varible of type struct dog
+/**
+ * init_dog - initialze a varible of type struct dog
  * @d:pointer for struct type
  * @name: pointer of string
  * @age: float type
  * @owner:string pointer
+ * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	} else
+		return;
 }
