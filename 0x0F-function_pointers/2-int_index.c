16c9c0b7bba1bce2d099771ec25e37ef397ff946
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-29 14:30:50 -0700

    handle null array and function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
