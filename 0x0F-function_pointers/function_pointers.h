77e0483fb3857adda23b781420dfee0108f264f1
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-29 14:20:20 -0700

    correct double inclusion problem and handle null function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -8,3 +10,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
