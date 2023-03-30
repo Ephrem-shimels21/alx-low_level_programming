b75131bc0264e883d16a196160ef1c2377123a2e
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-30 15:29:30 -0700

    create a function adds a new node

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -1,21 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - Calculate the number of elements.
- * @h: Pointer to a list.
- * Return: Integer.
- **/
-
-size_t list_len(const list_t *h)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	const list_t *temp;
-	unsigned int counter = 0;
+    list_t *head;
 
-	temp = h;
-	while (temp)
-	{
-		counter++;
-		temp = temp->next;
-	}
-	return (counter);
+    head = NULL;
+    add_node(&head, "Alexandro");
+    add_node(&head, "Asaia");
+    add_node(&head, "Augustin");
+    add_node(&head, "Bennett");
+    add_node(&head, "Bilal");
+    add_node(&head, "Chandler");
+    add_node(&head, "Damian");
+    add_node(&head, "Daniel");
+    add_node(&head, "Dora");
+    add_node(&head, "Electra");
+    add_node(&head, "Gloria");
+    add_node(&head, "Joe");
+    add_node(&head, "John");
+    add_node(&head, "John");
+    add_node(&head, "Josquin");
+    add_node(&head, "Kris");
+    add_node(&head, "Marine");
+    add_node(&head, "Mason");
+    add_node(&head, "Praylin");
+    add_node(&head, "Rick");
+    add_node(&head, "Rick");
+    add_node(&head, "Rona");
+    add_node(&head, "Siphan");
+    add_node(&head, "Sravanthi");
+    add_node(&head, "Steven");
+    add_node(&head, "Tasneem");
+    add_node(&head, "William");
+    add_node(&head, "Zee");
+    print_list(head);
+    return (0);
 }
