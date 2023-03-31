1d79efd1613ecdb69297e68ed06985b29e5ee051
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-31 14:14:57 -0700

    reduce number of characters per line at line 12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void myfun(void)
 {
 	void myfun(void) __attribute__ ((constructor));
 
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("I bore my house upon my back!\n");
 }
