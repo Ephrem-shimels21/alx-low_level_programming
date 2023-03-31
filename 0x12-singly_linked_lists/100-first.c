c7116840bba9540f304dc1dbb8818f04ad883acc
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-31 14:09:55 -0700

    create a function which prints its content before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * myfun - prints sentence
+ * Return: nothing
+ */
+
+void myfun(void)
+{
+	void myfun(void) __attribute__ ((constructor));
+
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
