fb04ff43d8276b8f404946ccc9d8657495ee395b
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-31 13:54:32 -0700

    remove double inclusion problems

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include<stddef.h>
 #include<stdlib.h>
 #include <string.h>
@@ -23,4 +26,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+#endif
