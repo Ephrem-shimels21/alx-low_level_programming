83131a97223535926b1cf612b2ceac37600c03c1
Ephrem-shimels21 <eph21sz@gmail.com>
2023-04-26 15:32:20 -0700

    correct erros

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,23 @@
 #include "main.h"
-#include "string.h"
-#include "math.h"
 
 /**
- * binary_to_uint - converts binary number ot an integer
- * @b: pointer to binary String
- * Return: integer
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int num = 0;
+	unsigned int dec_val = 0;
 
-	int len = strlen(b);
+	if (!b)
+		return (0);
 
-	for(i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '1')
-		{
-			num = num + pow(2,len);
-		}
-		len = len - 1;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
-	return (num);
+	return (dec_val);
 }
-
-
-
