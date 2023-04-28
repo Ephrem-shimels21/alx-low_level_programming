f59f442f97e4776408bf1349998af9e906a67b05
Ephrem-shimels21 <eph21sz@gmail.com>
2023-04-28 16:39:46 -0700

    correct double inclusion error in c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int get_endianness(void);
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -6,3 +8,4 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#endif
